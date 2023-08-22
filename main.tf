eb1884239e4b2fad2959f9c16d229fbe68117876
Jonathon Tait <jttait@gmail.com>
2023-08-22 08:36:34 +0100

    add formatting error for check

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "mediawiki" {
     description = "Allow inbound SSH"
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
+    protocol      = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
